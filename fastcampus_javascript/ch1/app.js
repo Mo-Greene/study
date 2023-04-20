8f97dda2fd5cf4ea950477631bd5f9e8d14695b4
Mo-Greene <13blueboy13@daum.net>
2023-04-20 15:52:53 +0900

    study vue

--- fastcampus_javascript/ch1/app.js
+++ fastcampus_javascript/ch1/app.js
@@ -3,7 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-//false는 동기적으로 가져온다는 의미
+const store = {
+    currentPage: 1,
+}
 
 /**
  * ajax Get Data
@@ -22,30 +24,36 @@ function newsFeed() {
     const newsList = [];
 
     newsList.push('<ul>');
-    for(let i = 0; i < 10; i++) {
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
+                <a href="#/show/${newsFeed[i].id}">
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
         `);
     }
     
-    newsList.push('</ul>')
+    newsList.push('</ul>');
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전페이지</a>
+            <a href="#/page/${store.currentPage + 1}">다음페이지</a>
+        </div>
+    `);
     container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
     //location == 주소와 관련된 객체
-    const id = location.hash.substring(1);
+    const id = location.hash.substring(7);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href="#">목록</a>
+            <a href="#/page/${store.currentPage}">목록</a>
         </div>
     `;
 }
@@ -54,6 +62,9 @@ function router() {
     const routePath = location.hash;
 
     if (routePath === '') {
+        newsFeed();
+    } else if(routePath.indexOf('#/page/') >= 0){
+        store.currentPage = Number(routePath.substring(7));
         newsFeed();
     } else {
         newsDetail();
