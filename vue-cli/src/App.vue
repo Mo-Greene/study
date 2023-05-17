d6f0a2eff72f3bfa65d7991d498e8832d3e60aad
Mo-Greene <13blueboy13@daum.net>
2023-05-17 18:20:37 +0900

    뷰터디 props 활용

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <User/>
+</template>
+
+<script>
+import User from '@/components/User.vue'
+
+export default {
+    name: "App",
+    data: () => ({
+        title: "Hello World",
+        count: 0
+    }),
+    components: { User }
+}
+</script>
---
+++ vue-cli/src/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ homeTitle }}</h1>
+</template>
+
+<script>
+import HomeView from './views/HomeView.vue';
+
+
+export default {
+    data: () => ({
+
+        homeTitle : "home page"
+    })
+}
+</script>
