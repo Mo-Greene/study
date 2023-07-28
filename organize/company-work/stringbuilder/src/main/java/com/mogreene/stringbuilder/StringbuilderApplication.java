5755f58a92f6ed2795011b601b46556d2e0bd96e
Mo-Greene <13blueboy13@daum.net>
2023-07-28 21:39:01 +0900

    c 언어 공부

---
+++ organize/company-work/stringbuilder/src/main/java/com/mogreene/stringbuilder/StringbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.mogreene.stringbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringbuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringbuilderApplication.class, args);
+    }
+
+}
---
+++ organize/company-work/stringbuilder/src/test/java/com/mogreene/stringbuilder/StringbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogreene.stringbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringbuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
